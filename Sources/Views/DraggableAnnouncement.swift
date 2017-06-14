c5895a23c272b1d2ca71ed20fabfd5907024e9a2
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2017-06-13 23:31:07 -0300

    Add DraggableAnnouncement and Delegate

---
+++ Sources/Views/DraggableAnnouncement.swift
@@ -0,0 +1,17 @@
+//
+//  DraggableAnnouncement.swift
+//  Announce
+//
+//  Created by Francesco Perrotti-Garcia on 6/13/17.
+//  Copyright © 2017 Announce. All rights reserved.
+//
+
+import UIKit
+
+protocol DraggableAnnouncement {
+    weak var draggableAnnouncementDelegate: DraggableAnnouncementDelegate? { get set }
+}
+
+protocol DraggableAnnouncementDelegate: class {
+    func boundsDidUpdate(_ newBounds: CGRect)
+}
